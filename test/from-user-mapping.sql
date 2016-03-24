3f364aece35fdb6872a5d6b652e2c14f91f903f2
Euler Taveira <euler@timbira.com.br>
2016-03-23 23:33:19 -0300

    Add some user mapping tests

---
+++ test/from-user-mapping.sql
@@ -0,0 +1,2 @@
+CREATE USER MAPPING FOR same_role_1 SERVER server1 OPTIONS(user 'mane', password 'senha');
+CREATE USER MAPPING FOR same_role_3 SERVER server3;
---
+++ test/to-user-mapping.sql
@@ -0,0 +1,2 @@
+CREATE USER MAPPING FOR same_role_1 SERVER server1 OPTIONS(user 'maria', password 'test');
+CREATE USER MAPPING FOR same_role_2 SERVER server2 OPTIONS(user 'eva');
