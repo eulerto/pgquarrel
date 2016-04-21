9682846f5d7de19051e69c5a6f48864002407aba
Euler Taveira <euler@timbira.com.br>
2016-04-21 00:14:10 -0300

    LD_LIBRARY_PATH: another try
    Set library path only while executing pgquarrel.

--- test/travis/run-tests.sh
+++ test/travis/run-tests.sh
@@ -5,18 +5,17 @@ PGPATH1=$HOME/$PGV/bin
 PGPATH2=$HOME/$PGV/bin
 PGPORT1=9901
 PGPORT2=9902
-LD_LIBRARY_PATH=$HOME/$PGV/lib:$HOME/pgquarrel/lib:$LD_LIBRARY_PATH
 # test needs a relative path
 cd pgquarrel/test
 # loading quarrel data
 $PGPATH1/psql -p $PGPORT1 -X -f test-server1.sql postgres
 $PGPATH2/psql -p $PGPORT2 -X -f test-server2.sql postgres
 # run pgquarrel
-$HOME/pgquarrel/bin/pgquarrel -c test.ini
+LD_LIBRARY_PATH=$HOME/$PGV/lib:$HOME/pgquarrel/lib:$LD_LIBRARY_PATH $HOME/pgquarrel/bin/pgquarrel -c test.ini
 # apply differences
 $PGPATH1/psql -p $PGPORT1 -X -f /tmp/test.sql quarrel1
 # test again
-$HOME/pgquarrel/bin/pgquarrel -c test2.ini
+LD_LIBRARY_PATH=$HOME/$PGV/lib:$HOME/pgquarrel/lib:$LD_LIBRARY_PATH $HOME/pgquarrel/bin/pgquarrel -c test2.ini
 # comparing dumps
 $PGPATH1/pg_dump -s -p $PGPORT1 -f /tmp/q1.sql quarrel1
 $PGPATH2/pg_dump -s -p $PGPORT2 -f /tmp/q2.sql quarrel2
