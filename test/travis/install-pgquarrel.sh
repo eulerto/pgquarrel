4adc139258bdc31ec4a1998f23c3071ae0a5e4ae
Euler Taveira <euler@timbira.com.br>
2018-05-13 14:55:34 -0300

    Another attempt to fix PostgreSQL path

--- test/travis/install-pgquarrel.sh
+++ test/travis/install-pgquarrel.sh
@@ -5,7 +5,7 @@ PGV=$1
 if [ $2 -gt $1 ]; then
 	PGV=$2
 fi
-PGPREFIX=/usr/lib/postgresql/$PGV/bin
+PGPREFIX=/usr/include/postgresql/$PGV/server
 git clone https://github.com/eulerto/pgquarrel.git
 cd pgquarrel
 cmake -DCMAKE_INSTALL_PREFIX=$HOME/pgquarrel -DCMAKE_PREFIX_PATH=$PGPREFIX .
