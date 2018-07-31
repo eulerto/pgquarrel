98a973fa8a5dd24c39cb782e116a1a5c2663955b
Euler Taveira <euler@timbira.com.br>
2018-07-31 12:24:36 -0300

    Run pg_config
    Provides information about the installed version of PostgreSQL.

--- test/travis/install-pgquarrel.sh
+++ test/travis/install-pgquarrel.sh
@@ -10,4 +10,5 @@ git clone https://github.com/eulerto/pgquarrel.git
 cd pgquarrel
 cmake -DCMAKE_INSTALL_PREFIX=$HOME/pgquarrel -DCMAKE_PREFIX_PATH=$PGPREFIX .
 find /usr -name 'libpgport.a' -print
+/usr/lib/postgresql/10/bin/pg_config
 make VERBOSE=1 install
