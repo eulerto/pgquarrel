b5531c1c4832cf2db63c4fae8f6adc3a9d5d3900
Euler Taveira <euler@timbira.com.br>
2019-02-08 00:41:34 +0000

    Fix tests
    We don't need to set CMAKE_PREFIX_PATH since commit 5ddf6a8 (thanks to
    autodetect code). I also update tests to version 11.

--- test/travis/install-pgquarrel.sh
+++ test/travis/install-pgquarrel.sh
@@ -5,10 +5,7 @@ PGV=$1
 if [ $2 -gt $1 ]; then
 	PGV=$2
 fi
-PGPREFIX=/usr/include/postgresql/$PGV/server
 git clone https://github.com/eulerto/pgquarrel.git
 cd pgquarrel
-cmake -DCMAKE_INSTALL_PREFIX=$HOME/pgquarrel -DCMAKE_PREFIX_PATH=$PGPREFIX .
-find /usr -name 'libpgport.a' -print
-/usr/lib/postgresql/10/bin/pg_config
+cmake -DCMAKE_INSTALL_PREFIX=$HOME/pgquarrel .
 make VERBOSE=1 install
